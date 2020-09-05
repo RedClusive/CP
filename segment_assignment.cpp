a8c1f3982a2b700e24c1e69c4ab9933f2acf00b1
Ivan Novitskiy <40142837+RedClusive@users.noreply.github.com>
2020-09-05 21:01:09 +0300

    Update segment_assignment.cpp

--- segment_assignment.cpp
+++ segment_assignment.cpp
@@ -4,30 +4,33 @@ struct segtree {
     vector<T> t;
     vector<T> lazy;
     vector<bool> need;
+    
     segtree(int sz) {
         n = 1;
-        while (n < sz) n *= 2;
-        t.resize(2 * n, 0);
-        lazy.resize(2 * n, 0);
-        need.resize(2 * n, 0);
+        while (n < sz) n <<= 1;
+        t.resize(n << 1, 0);
+        lazy.resize(n << 1, 0);
+        need.resize(n << 1, 0);
     }
-    T op(T a, T b) {
+
+    T op(const T& a, const T& b) {
         return a + b;
     }
+
     void push(int v, int l, int r) {
         if (need[v] && v < n) {
-            int mid = (l + r) >> 1;
-            t[v * 2] = lazy[v] * (mid - l);
-            t[v * 2 + 1] = lazy[v] * (r - mid);
-            lazy[v * 2] = lazy[v];
-            lazy[v * 2 + 1] = lazy[v];
-            need[v * 2] = need[v];
-            need[v * 2 + 1] = need[v];
+            int mid = (l + r) >> 1, lc = v << 1, rc = v << 1 | 1;
+            t[lc] = lazy[v] * (mid - l);
+            t[rc] = lazy[v] * (r - mid);
+            lazy[lc] = lazy[v];
+            lazy[rc] = lazy[v];
+            need[lc] = need[v];
+            need[rc] = need[v];
             lazy[v] = 0;
             need[v] = 0;
         }
     }
-    void upd(int v, int cl, int cr, int l, int r, T x) {
+    void upd(int v, int cl, int cr, int l, int r, const T& x) {
         if (r <= cl || cr <= l) return;
         if (l <= cl && cr <= r) {
             lazy[v] = x;
@@ -36,12 +39,12 @@ struct segtree {
             return;
         }
         push(v, cl, cr);
-        int mid = (cl + cr) / 2;
-        upd(v * 2, cl, mid, l, r, x);
-        upd(v * 2 + 1, mid, cr, l, r, x);
-        t[v] = op(t[v * 2], t[v * 2 + 1]);
+        int mid = (cl + cr) >> 1;
+        upd(v << 1, cl, mid, l, r, x);
+        upd(v << 1 | 1, mid, cr, l, r, x);
+        t[v] = op(t[v << 1], t[v << 1 | 1]);
     }
-    void upd(int l, int r, T x) {
+    void upd(int l, int r, const T& x) {
         upd(1, 0, n, l, r, x);
     }
     T get(int v, int cl, int cr, int l, int r) {
@@ -50,8 +53,8 @@ struct segtree {
             return t[v];
         }
         push(v, cl, cr);
-        int mid = (cl + cr) / 2;
-        return op(get(v * 2, cl, mid, l, r), get(v * 2 + 1, mid, cr, l, r));
+        int mid = (cl + cr) >> 1;
+        return op(get(v << 1, cl, mid, l, r), get(v << 1 | 1, mid, cr, l, r));
     }
     T get(int l, int r) {
         return get(1, 0, n, l, r);
